a3a923b5b8966e700b3120256e8fb4262e8cfb0d
MD-ZOHAIB-KHAN <zohaibtheprogrammer@gmail.com>
2025-10-16 19:00:00 +0500

    loader

--- src/component/loader/loader.jsx
+++ src/component/loader/loader.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-
-import collegeLogo from "../../assets/collegeLogo.png";
 import "./loader.css";
 
 const Loader = () => {
   return (
-    <div className="loaderContainer">
-      <img src={collegeLogo} alt="College Logo" className="loaderLogo " />
-
-      <h1 className="flex font-bold text-white">
-        Loading<span className="dots">...</span>
-      </h1>
-    </div>
+    <section class="dots-container">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </section>
   );
 };
 
--- src/pages/dashboard/feeSubmission.jsx
+++ src/pages/dashboard/feeSubmission.jsx
@@ -3,31 +3,42 @@ import "../style/department.css";
 import "../style/feeSubmission.css";
 import "../../constant/applicationStyle.css";
 import { useEffect, useState } from "react";
-import { feesTypes } from "../../constant/lists";
 import { useDispatch, useSelector } from "react-redux";
 import { getFees } from "../../store/Thunk/commonThunk";
 
 const FeeSubmission = () => {
   const [rollNo, setRollNo] = useState("");
   const [checkedItems, setCheckedItems] = useState({});
+  const [totalFee, setTotalFee] = useState(0);
 
   const dispatch = useDispatch();
   const location = useLocation();
   const selectedDepartment = location.state?.selectedDepartment;
   const feesList = useSelector((state) => state.common.fees);
+  console.log("feeslist: ", feesList);
 
   useEffect(() => {
     dispatch(getFees({ id: selectedDepartment?.deparment_id }));
-  }, []);
+  }, [dispatch, selectedDepartment]);
 
   const handleCheckBoxes = (e) => {
     const { name, checked } = e.target;
-    setCheckedItems((prev) => ({
-      ...prev,
-      [name]: checked,
-    }));
-  };
 
+    setCheckedItems((prev) => {
+      const updated = { ...prev, [name]: checked };
+
+      // Calculate total fee based on checked items
+      let newTotal = 0;
+      feesList.forEach((fee) => {
+        if (updated[fee.fees_name]) {
+          newTotal = newTotal + Number(fee.amount);
+        }
+      });
+
+      setTotalFee(newTotal);
+      return updated;
+    });
+  };
   console.log("Checked items:", checkedItems);
 
   // console.log("check box", checkedItems);
@@ -50,7 +61,7 @@ const FeeSubmission = () => {
         </div>
 
         {/* Fee Types checkboxes */}
-        <div className="flex flex-wrap gap-4 justify-center items-center w-full">
+        {/* <div className="flex flex-wrap gap-4 justify-center items-center w-full">
           {feesTypes.map((feeType, index) => (
             <div className="flex  flex-wrap gap-x-1 gap-y-3" key={index}>
               <input
@@ -65,6 +76,24 @@ const FeeSubmission = () => {
               </label>
             </div>
           ))}
+        </div> */}
+
+        {/* Fee Types checkboxes (from DB) */}
+        <div className="flex flex-wrap gap-4 justify-center items-center w-full mb-4">
+          {feesList.map((fee) => (
+            <div className="flex items-center gap-2" key={fee.id}>
+              <input
+                type="checkbox"
+                id={fee.fees_name}
+                name={fee.fees_name}
+                checked={!!checkedItems[fee.fees_name]}
+                onChange={handleCheckBoxes}
+              />
+              <label htmlFor={fee.fees_name} className="label capitalize">
+                {fee.fees_name.replace(/_/g, " ")}
+              </label>
+            </div>
+          ))}
         </div>
 
         {/* Total Fee Display */}
@@ -72,7 +101,7 @@ const FeeSubmission = () => {
           <label className="">Total Fee:</label>
           <input
             type="text"
-            value={""}
+            value={totalFee.toLocaleString()}
             disabled={true}
             className="rollnoInput"
           />
